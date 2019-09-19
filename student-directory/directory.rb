49aa5db827947a0529c5da7ff7bc02abacdc451d
Adam1824 <forrestadam07@gmail.com>
2019-09-19 17:47:48 +0100

    Make students accessible for all methods

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,26 +1,27 @@
 # let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Football", cob: "UK"},
-  {name: "Darth Vader", cohort: :november, hobbies: "programming", cob: "UK"},
-  {name: "Nurse Ratched", cohort: :november, hobbies: "Football", cob: "UK"},
-  {name: "Michael Corleone", cohort: :november, hobbies: "Gaming", cob: "UK"},
-  {name: "Alex DeLarge", cohort: :november, hobbies: "Gaming", cob: "UK"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "programming", cob: "UK"},
-  {name: "Terminator", cohort: :november, hobbies: "programming", cob: "UK"},
-  {name: "Freddy Krueger", cohort: :november, hobbies: "Gaming", cob: "UK"},
-  {name: "The Joker", cohort: :november, hobbies: "Football", cob: "UK"},
-  {name: "Joffrey Baratheon", cohort: :november, hobbies: "Gaming", cob: "UK"},
-  {name: "Norman Bates", cohort: :november, hobbies: "Gaming", cob: "UK"}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Football", cob: "UK"},
+#   {name: "Darth Vader", cohort: :november, hobbies: "programming", cob: "UK"},
+#   {name: "Nurse Ratched", cohort: :november, hobbies: "Football", cob: "UK"},
+#   {name: "Michael Corleone", cohort: :november, hobbies: "Gaming", cob: "UK"},
+#   {name: "Alex DeLarge", cohort: :november, hobbies: "Gaming", cob: "UK"},
+#   {name: "The Wicked Witch of the West", cohort: :november, hobbies: "programming", cob: "UK"},
+#   {name: "Terminator", cohort: :november, hobbies: "programming", cob: "UK"},
+#   {name: "Freddy Krueger", cohort: :november, hobbies: "Gaming", cob: "UK"},
+#   {name: "The Joker", cohort: :november, hobbies: "Football", cob: "UK"},
+#   {name: "Joffrey Baratheon", cohort: :november, hobbies: "Gaming", cob: "UK"},
+#   {name: "Norman Bates", cohort: :november, hobbies: "Gaming", cob: "UK"}
+#]
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
+def print_students
+  puts @students
   cohort_list = []
-  students.each do |student|
+  @students.each do |student|
     cohort = student[:cohort].to_s
     if !(cohort_list.include?(cohort))
       cohort_list.push(cohort)
@@ -28,7 +29,7 @@ def print(students)
   end
   cohort_list.each do |cohort|
     puts "Students from the #{cohort} cohort:"
-      students.each do |student|
+      @students.each do |student|
         if student[:cohort].to_s.include?(cohort)
           puts student[:name]
         end
@@ -36,15 +37,13 @@ def print(students)
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
   #get the first name
   name = gets.chomp
   cohort = gets.chomp
@@ -57,40 +56,47 @@ def input_students
     end
     cohort = cohort.to_sym
     #add the student hash to the array
-    students << {name: name, cohort: cohort}
-    if students.count == 1
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
       puts "Now we have 1 student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     #get another name from the user
     name = gets.chomp
     cohort = gets.chomp
   end
-  if students.count == 0
+  if @students.count == 0
     puts "We have no students"
   end
-  #return the array of students
-  return students
+  puts @students
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students
+  print_footer
 end
 
 def interactive_menu
-  students = []
+  @students = []
   loop do
     # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
+    print_menu
     # 2. read the input and save it into a variable
     selection = gets.chomp
     # 3. do what the user has asked
     case selection
       when "1"
-        students = input_students
+        input_students
       when "2"
-        print_header
-        print(students)
-        print_footer(students)
+        show_students
       when "9"
         exit # this will cause the program to terminate
       else
@@ -99,4 +105,4 @@ def interactive_menu
   end
 end
 
-interactive_menu()
+interactive_menu
